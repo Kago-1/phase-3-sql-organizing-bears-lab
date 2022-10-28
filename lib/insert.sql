706305c152385cb2a148eee1d25cc36dfcac7924
Steve <stephenkago10@gmail.com>
2022-10-28 03:25:31 +0300

    Added database

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate",2,"M","Black","Average","Yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",4,"F","Brown","Average","Yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",7,"M","Black","Extreme","Yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",7,"M","Black","Average","No");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",12,"F","Black","Average","No");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",9,"F","White","Mild","Yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",5,"M","White","Average","Yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,13,"M","Black","Cool","Yes");
