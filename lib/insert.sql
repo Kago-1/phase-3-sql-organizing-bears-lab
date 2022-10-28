d4d4833de3b540789d966522f3ad0aafe2ace1e9
Steve <stephenkago10@gmail.com>
2022-10-28 03:37:29 +0300

    Added database

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,9 @@
-INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate",2,"M","Black","Average","Yes");
-INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",4,"F","Brown","Average","Yes");
-INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",7,"M","Black","Extreme","Yes");
-INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",7,"M","Black","Average","No");
-INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",12,"F","Black","Average","No");
-INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",9,"F","White","Mild","Yes");
-INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",5,"M","White","Average","Yes");
-INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,13,"M","Black","Cool","Yes");
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES
+(1,"Mr.Chocolate",2,"M","Black","Average",1),
+(2,"Rowdy",4,"F","Brown","Average",1),
+(3,"Tabitha",7,"M","Black","Extreme",1),
+(4,"Sergeant Brown",7,"M","Black","Average",2),
+(5,"Melissa",12,"F","Black","Average",2),
+(6,"Grinch",9,"F","White","Mild",1),
+(7,"Wendy",5,"M","White","Average",1),
+(8,NULL,13,"M","Black","Cool",1);
